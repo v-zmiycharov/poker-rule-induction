a25bcecbb44b6023ec7e36ac1786792c7e97f711
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2015-01-31 14:15:27 +0200

    Save rules to dictionary

--- src/PokerRuleInduction/Helpers.cs
+++ src/PokerRuleInduction/Helpers.cs
@@ -9,6 +9,10 @@ namespace PokerRuleInduction
 {
     public static class Helpers
     {
+        public static Dictionary<int, List<OrderedCardsRule>> HandOrderedCardsDict = new Dictionary<int,List<OrderedCardsRule>>();
+        public static Dictionary<int, List<SameRankRule>> HandSameRankDict = new Dictionary<int,List<SameRankRule>>();
+        public static Dictionary<int, List<SameSuitRule>> HandSameSuitDict = new Dictionary<int,List<SameSuitRule>>();
+
         public static void ReadTrainData()
         {
             string projectDir = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
@@ -20,8 +24,18 @@ namespace PokerRuleInduction
             while ((line = file.ReadLine()) != null)
             {
                 var hand = new PokerHand(line);
-                Debug.WriteLine(hand.ToString());
-                Debug.WriteLine(hand.GetRulesInfo());
+                
+                if(!HandOrderedCardsDict.ContainsKey(hand.Hand.Value))
+                    HandOrderedCardsDict.Add(hand.Hand.Value, new List<OrderedCardsRule>());
+                HandOrderedCardsDict[hand.Hand.Value].AddRange(hand.GetOrderedCardsRules());
+                
+                if(!HandSameRankDict.ContainsKey(hand.Hand.Value))
+                    HandSameRankDict.Add(hand.Hand.Value, new List<SameRankRule>());
+                HandSameRankDict[hand.Hand.Value].AddRange(hand.GetSameRankRules());
+                
+                if(!HandSameSuitDict.ContainsKey(hand.Hand.Value))
+                    HandSameSuitDict.Add(hand.Hand.Value, new List<SameSuitRule>());
+                HandSameSuitDict[hand.Hand.Value].AddRange(hand.GetSameSuitRules());
             }
         }
     }
